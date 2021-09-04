374e149cdec70ea7b0ed76646b181de82dd7bfa0
SaLeah1 <90078536+SaLeah1@users.noreply.github.com>
2021-09-03 23:03:31 -0400

    Add files via upload

---
+++ blaseball as popups pys.py
@@ -0,0 +1,25 @@
+from sseclient import SSEClient
+import json
+import time
+windowsvista = open("windowsvista.txt",'w')
+while True:
+    try:
+        stream = SSEClient('https://api.sibr.dev/replay/v1/replay?from=2020-08-30T01:00:08.17Z')
+        for message in stream:
+            # At seemingly fixed intervals, the stream sends an empty message
+            if not str(message):
+                continue
+            data = json.loads(str(message))
+            # Sometimes the stream just sends fights
+            if 'games' not in data['value']:
+                continue
+            # If those checks passed, this should work
+            games = json.loads(str(message))['value']['games']['schedule']
+            for game in games:
+                feed = game['lastUpdate']
+                windowsvista.write(feed)
+                windowsvista.write('\n')
+    except Exception as error:
+        print(error)
+        # Wait five minutes if the stream breaks
+        time.sleep(300)
